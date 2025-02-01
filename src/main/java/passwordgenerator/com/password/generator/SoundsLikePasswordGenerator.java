8af40debaef78938558dd5ad359d435b5d110dec
KacperTetela <kacpertetela1@gmail.com>
2025-02-01 22:25:25 +0100

    Refactoring SoundsLikePasswordGenerator

--- src/main/java/passwordgenerator/com/password/generator/SoundsLikePasswordGenerator.java
+++ src/main/java/passwordgenerator/com/password/generator/SoundsLikePasswordGenerator.java
@@ -1,5 +1,6 @@
 package passwordgenerator.com.password.generator;
 
+import passwordgenerator.com.common.Utils;
 import passwordgenerator.com.common.words.APIWordDownloader;
 import passwordgenerator.com.common.words.WordAPIParams;
 import passwordgenerator.com.password.PasswordCriteria;
@@ -10,19 +11,39 @@ public class SoundsLikePasswordGenerator implements PasswordGenerator {
 
     @Override
     public String generatePassword(PasswordCriteria passwordCriteria) {
+        StringBuilder passwordToReturn = new StringBuilder();
+        int length = passwordCriteria.digits() ? passwordCriteria.length() - 1 : passwordCriteria.length();
+        length = passwordCriteria.special() ? length - 1 : length;
 
-        String potentialPassword;
         try {
-            potentialPassword = new APIWordDownloader().getFromAPI(new WordAPIParams.Builder()
+            passwordToReturn.append(new APIWordDownloader().getFromAPI(new WordAPIParams.Builder()
                     .soundsLike(passwordCriteria.relatedWord())
-                    .length(passwordCriteria.length())
-                    .build());
+                    .length(length)
+                    .build()));
 
         } catch (Exception e) {
-            potentialPassword = relatedWord;
+            passwordToReturn.append(relatedWord);
         }
 
-        return potentialPassword;
+        //replace first letter to uppercase
+        if (passwordCriteria.uppercase()) {
+            passwordToReturn = passwordToReturn.replace(0, 1, passwordToReturn.substring(0, 1).toUpperCase());
+        }
+
+        //add digit
+        if (passwordCriteria.digits()) {
+            //passwordToReturn.deleteCharAt(passwordToReturn.length() - 1);
+            passwordToReturn.append(PasswordCriteria.DIGITS.get(Utils.getRandomIndex(10)));
+        }
+
+        //add special sign
+        if (passwordCriteria.special()) {
+            passwordToReturn.append(PasswordCriteria.SPECIAL_SIGNS.get(Utils.getRandomIndex(PasswordCriteria.SPECIAL_SIGNS.size())));
+        }
+
+        // Password should contain _ instead of spaces
+        return passwordToReturn.toString()
+                .replace(" ", "_");
     }
 
 }
